d2ceb1f6c297696639d879809b0d559cf9736d24
kjong37-max <kjong37@gmail.com>
2026-05-08 15:12:49 +0900

    Revert "feat: Phase 9 더미 데이터 및 gMock 단위 테스트 구현"
    This reverts commit 0b9851fa066bca94bfa65636d364fb7180398b99.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,10 @@
 ﻿#define NOMINMAX
 #include <Windows.h>
-#include <gtest/gtest.h>
 #include "Controller/MainController.h"
 
-// 실행 방법:
-//   SampleOrderSystem.exe          → 애플리케이션 실행
-//   SampleOrderSystem.exe --test   → gMock 단위 테스트 실행
-int main(int argc, char** argv) {
+int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
-
-    for (int i = 1; i < argc; ++i) {
-        if (std::string(argv[i]) == "--test") {
-            ::testing::InitGoogleTest(&argc, argv);
-            return RUN_ALL_TESTS();
-        }
-    }
 
     MainController controller;
     controller.run();
